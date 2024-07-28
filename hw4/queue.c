fb881ec70d7bcd8a3c69f01f5da831f8659c989f
Eyal Roginski <eyalrog1@gmail.com>
2024-07-28 19:51:21 +0300

    Finished! Using manager thread

--- hw4/queue.c
+++ hw4/queue.c
@@ -1,4 +1,6 @@
 #include "queue.h"
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <threads.h>
 
@@ -20,8 +22,12 @@ typedef struct simple_queue_t {
 typedef struct queue_t {
     simple_queue_t data_queue;
     simple_queue_t waiting_queue;
-    mtx_t primary_mutex;
-    mtx_t secondary_mutex;
+    mtx_t mutex;
+    thrd_t manager_thread;
+    cnd_t manager_cond;
+    bool awake_waiting; // Is there a thread that's passed the dequeue queue and
+                        // been woken up, but hasn't finished dequeuing yet?
+    bool active;
 } queue_t;
 
 queue_t queue;
@@ -34,6 +40,7 @@ void wait_on_queue(void);
 void release_waiting_on_queue(void);
 void free_cond_queue(simple_queue_t *simple_queue);
 void destroy_simple_queue(simple_queue_t *simple_queue);
+int manager(void *arg);
 
 void init_simple_queue(simple_queue_t *simple_queue) {
     simple_queue->start.next = NULL;
@@ -56,7 +63,7 @@ void simple_enqueue(simple_queue_t *simple_queue, void *item) {
 
 /**
  * Dequeues an item from the simple_queue, and returns its value. Do not call if
- * queue is empty!
+ * queue is empty (segfault)!
  * */
 void *simple_dequeue(simple_queue_t *simple_queue) {
     node_t *removed_node = simple_queue->start.next;
@@ -82,8 +89,11 @@ bool is_empty(simple_queue_t *simple_queue) {
 void initQueue(void) {
     init_simple_queue(&queue.data_queue);
     init_simple_queue(&queue.waiting_queue);
-    mtx_init(&queue.primary_mutex, mtx_plain);
-    mtx_init(&queue.secondary_mutex, mtx_plain);
+    mtx_init(&queue.mutex, mtx_plain);
+    cnd_init(&queue.manager_cond);
+    queue.active = true;
+    queue.awake_waiting = false;
+    thrd_create(&queue.manager_thread, manager, NULL);
 }
 
 void free_cond_queue(simple_queue_t *simple_queue) {
@@ -104,72 +114,101 @@ void destroy_simple_queue(simple_queue_t *simple_queue) {
 }
 
 void destroyQueue(void) {
+    queue.active = false;
+    cnd_signal(&queue.manager_cond);
+    thrd_join(queue.manager_thread, NULL);
+    mtx_lock(&queue.mutex);
     free_cond_queue(&queue.waiting_queue);
     destroy_simple_queue(&queue.waiting_queue);
     destroy_simple_queue(&queue.data_queue);
+    cnd_destroy(&queue.manager_cond);
+    mtx_destroy(&queue.mutex);
 }
 
 void enqueue(void *item) {
-    mtx_lock(&queue.primary_mutex);
-    mtx_lock(&queue.secondary_mutex);
+    if (!queue.active) {
+        return;
+    }
+    mtx_lock(&queue.mutex);
     simple_enqueue(&queue.data_queue, item);
-    release_waiting_on_queue();
-    mtx_unlock(&queue.primary_mutex);
+    cnd_signal(&queue.manager_cond);
+    mtx_unlock(&queue.mutex);
 }
 
 /**
- * Blocks until a new item is enqueued. queue.mutex should be held
+ * Enters the queue of threads waiting to dequeue from the data structure, and
+ * blocks until it is woken up by the manager thread. queue.mutex should be held
  * when this is called, and is still held when this returns.
  * */
 void wait_on_queue(void) {
     cnd_t *cond = (cnd_t *)malloc(sizeof(cnd_t));
     cnd_init(cond);
+    cnd_signal(&queue.manager_cond);
     simple_enqueue(&queue.waiting_queue, (void *)cond);
-    cnd_wait(cond, &queue.secondary_mutex);
+    cnd_wait(cond, &queue.mutex);
 }
 
 /**
- * Release the next `dequeue` waiting for an item. queue.primary_mutex should be
- * held. Does nothing if there are no threads waiting.
+ * Release the next `dequeue` waiting for an item. queue.mutex should be
+ * held. Does nothing if there are no threads waiting, or if there is no data in
+ * the queue.
  * */
 void release_waiting_on_queue(void) {
-    if (is_empty(&queue.waiting_queue)) {
-        mtx_unlock(&queue.secondary_mutex);
-        return;
+    if (!queue.awake_waiting && !is_empty(&queue.data_queue) &&
+        !is_empty(&queue.waiting_queue)) {
+        cnd_t *cond = simple_dequeue(&queue.waiting_queue);
+        cnd_signal(cond);
+        queue.awake_waiting = true;
+        cnd_destroy(cond);
+        free(cond);
     }
+}
 
-    cnd_t *cond = simple_dequeue(&queue.waiting_queue);
-    cnd_signal(cond);
-    mtx_unlock(&queue.secondary_mutex);
-    cnd_destroy(cond);
-    free(cond);
+/**
+ * Manager thread function. Waits to be signaled, whereupon it releases a single
+ * thread waiting to dequeue.
+ * */
+int manager(void *arg) {
+    mtx_lock(&queue.mutex);
+    while (queue.active) {
+        release_waiting_on_queue();
+        cnd_wait(&queue.manager_cond, &queue.mutex);
+    }
+    mtx_unlock(&queue.mutex);
+    return 0;
 }
 
 void *dequeue(void) {
-    mtx_lock(&queue.primary_mutex);
-    mtx_lock(&queue.secondary_mutex);
-    mtx_unlock(&queue.primary_mutex);
-    if (is_empty(&queue.data_queue)) {
-        // Queue is empty, wait until an item arrives.
-        wait_on_queue();
+    if (!queue.active) {
+        return NULL;
     }
-    // Queue isn't empty!
+    mtx_lock(&queue.mutex);
+    wait_on_queue();
     void *item = simple_dequeue(&queue.data_queue);
-    mtx_unlock(&queue.secondary_mutex);
+    queue.awake_waiting = false;
+    cnd_signal(&queue.manager_cond);
+    mtx_unlock(&queue.mutex);
     return item;
 }
 
 bool tryDequeue(void **item) {
-    bool result = false;
-    mtx_lock(&queue.primary_mutex);
-    mtx_lock(&queue.secondary_mutex);
-    if (!is_empty(&queue.data_queue)) {
-        result = true;
-        *item = simple_dequeue(&queue.data_queue);
+    if (!queue.active) {
+        return false;
     }
-    mtx_unlock(&queue.secondary_mutex);
-    mtx_unlock(&queue.primary_mutex);
-    return result;
+
+    mtx_lock(&queue.mutex);
+    while (!is_empty(&queue.data_queue) && !is_empty(&queue.waiting_queue)) {
+        cnd_signal(&queue.manager_cond);
+        mtx_unlock(&queue.mutex);
+        mtx_lock(&queue.mutex);
+    }
+    if (!is_empty(&queue.data_queue)) {
+        *item = simple_dequeue(&queue.data_queue);
+        mtx_unlock(&queue.mutex);
+        return true;
+    }
+    mtx_unlock(&queue.mutex);
+    return false;
 }
 
 size_t size(void) { return queue.data_queue.size; }
